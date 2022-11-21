00735e03ab0dff59442ce0411121c26021bb31b3
Patrick <patrickburns2557@gmail.com>
2022-11-20 18:01:10 -0600

    files get added to list and deleted

--- burns_cs390program4.pl
+++ burns_cs390program4.pl
@@ -2,8 +2,8 @@
 
 if(@ARGV)
 {
-    @file_list = @ARGV;
-    @file_list = glob("@file_list");
+    @file_list = @ARGV; # get the file list from the command arguments
+    @file_list = glob("@file_list"); # expand any wildcards to include all valid files
 }
 else
 {
@@ -11,16 +11,76 @@ else
     exit 1;
 }
 
+#@remove_list;
+
+print("Enter 'y' to select the file to remove.\n");
+print("Enter 'q' to quit file prompt.\n");
+print("Enter anything else to skip file.\n");
+
 foreach(@file_list)
 {
-    print("$_\n");
+    
+    if (-d $_) #if directory file
+    {
+        print("Delete? [y,q] Directory: $_\n");
+    }
+    else #if regular file
+    {
+        print("Delete? [y,q] File: $_\n");
+    }
+
+    $input = <STDIN>;
+    
+    if($input eq "q\n" or $input eq "Q\n")
+    {
+        last; #break
+    }
+    if($input eq "y\n" or $input eq "Y\n")
+    {
+        push(@remove_list, $_);
+    }
 }
+
+print "\n\n\nFILES TO REMOVE: \n";
+#iterate through the indexes, so the indexes can be printed w/o a separate counter
+for my $i (0 .. $#remove_list)
+{
+    print "[$i]: $remove_list[$i]\n";
+}
+
+
 print "Perform all deletions? [y]: ";
 $input = <STDIN>;
-print "$input";
 
-#delete
-
-
+if($input eq "y\n" or $input eq "Y\n")
+{
+    print "DELETION RESULTS:\n";
+    #iterate through the indexes, so the indexes can be printed w/o a separate counter
+    for my $i (0 .. $#remove_list)
+    {
+        if (-d $remove_list[$i]) #directory
+        {
+            rmdir("$remove_list[$i]");
+            if (-d $remove_list[$i]) #If the foler still exists, it wasn't empty
+            {
+                print("  [$i]: $remove_list[$i] is not empty, cannot delete.\n");
+            }
+            else
+            {
+                print("  [$i]: $remove_list[$i] deleted.\n");
+            }
+        }
+        else #file
+        {
+            unlink("$remove_list[$i]");
+            print("  [$i]: $remove_list[$i] deleted.\n");
+        }
+        
+    }
+}
+else
+{
+    print("Nothing has been deleted.\n");
+}
 
 exit 0;
